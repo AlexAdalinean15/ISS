e90ab6ac26ca8d862d2fc771215835e17070307b
GoiaIulian <goia.iulian@yahoo.com>
2020-05-21 21:03:35 +0300

    minor fixes

--- ISSMainPage.js
+++ ISSMainPage.js
@@ -44,7 +44,7 @@ function popup(carId, man, model, nrSeats, price)
 {
     var str = "<div class='Popup'>";
     str += getCarPhoto(carId);
-    str += " <p>" + man + " "+ model + "</p> <p><b>Seats:</b> " + nrSeats + "</p> <p><b>Price:</b> " + price + "$</p>";
+    str += " <p>" + man + " "+ model + "</p> <p><b>Seats:</b> " + nrSeats + "</p> <p><b>Price:</b> " + price + " $</p>";
     str += "<button class='Rent' id='" + carId + "'onClick='rent(this.id)'>Rent</button></div>";
     return str;
 }
@@ -89,7 +89,7 @@ function getCarPhoto(id)
 
         else
         {
-            alert("Invalid username or password!");
+            alert("Could not get cars!");
         }
 }
 
